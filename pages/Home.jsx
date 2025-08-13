0016b79ee68744049084df2fe071a9357ce6e781
wpierce19 <126428398+wpierce19@users.noreply.github.com>
2025-08-12 18:27:53 -0600

    Adjusting theme for editor

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -3,36 +3,31 @@ import TiptapEditor from "../src/features/post/editor/TipTapEditor";
 import { SimpleEditor } from '@/components/tiptap-templates/simple/simple-editor'
 
 const Home = () => {
-    const [html, setHtml] = useState('');
+    //const [html, setHtml] = useState('');
 
     return (
-        <div className="grid grid-cols-6 min-h-screen pt-1">
-            <div className="border border-amber-300 col-span-1 flex justify-center items-start">
-                <h1>Online Friends</h1>
-            </div>
-            <div 
-            className="border border-amber-300 col-span-4 grid grid-rows-[1fr_4fr]"
-            >
-                <div className="border border-amber-300 flex items-center justify-center px-4">
-                    <h1>Welcome stuff</h1>
+        <div>
+            <div className="grid grid-cols-6 min-h-screen pt-1">
+                <div className="border border-amber-300 col-span-1 flex justify-center items-start">
+                    <h1>Online Friends</h1>
                 </div>
-                <div className="border border-amber-300 px-4 flex justify-center">
-                    <h1>FEED</h1>
-                </div>
-            </div>
-            <div className="border border-amber-300 col-span-1 min-h-screen px-4 flex justify-center items-start">
-                <h2>Direct Messages</h2>
-            </div>
-            <h1>Testing editor</h1>
-            <SimpleEditor />
-            <div className="max-w-2xl mx-auto p-4 w-full">
-                <TiptapEditor value={html} onChange={setHtml} />
-                <button
-                    className="mt-4 px-4 py-2 rounded bg-blue-600 text-white"
-                    onClick={() => console.log('submit:', html)}
+                <div 
+                className="border border-amber-300 col-span-4 grid grid-rows-[1fr_4fr]"
                 >
-                    Submit
-                </button>
+                    <div className="border border-amber-300 flex items-center justify-center px-4">
+                        <h1>Welcome stuff</h1>
+                    </div>
+                    <div className="border border-amber-300 px-4 flex justify-center">
+                        <h1>FEED</h1>
+                    </div>
+                </div>
+                <div className="border border-amber-300 col-span-1 min-h-screen px-4 flex justify-center items-start">
+                    <h2>Direct Messages</h2>
+                </div>
+            </div>
+            <div className="rounded-lg border border-gray-300 dark:border-gray-700 bg-white dark:bg-zinc-900 w-full max-w-2xl min-h-[20rem] shadow-sm p-4">
+                <p>Testing Editor</p>
+                <SimpleEditor />
             </div>
         </div>
     );
