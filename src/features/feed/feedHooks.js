25407aa13952630f80cd6404172518954282929a
wpierce19 <126428398+wpierce19@users.noreply.github.com>
2025-10-03 13:59:50 -0600

    created query hooks for post creating / fetching

--- src/features/feed/feedHooks.js
+++ src/features/feed/feedHooks.js
@@ -1,1 +1,25 @@
 //Place queryhooks here for feed fetching
+import { useQuery, useMutation } from '@tanstack/react-query';
+import { getFriendsPosts, getPosts, createPost } from './feedApi';
+
+export const useFriendsPosts = () => {
+    return useQuery({
+        queryKey: ['friendsPosts'],
+        queryFn: () => getFriendsPosts(),
+        staleTime: 5 * 60 * 1000, // 5 minutes
+        refetchOnWindowFocus: false,
+    })
+}
+
+export const usePosts = () => {
+    return useQuery({
+        queryKey: ['posts'],
+        queryFn: () => getPosts(),
+    })
+}
+
+export const useCreatePost = (newPost) => {
+    return useMutation({
+        mutationFn: () => createPost(newPost),
+    })
+}
