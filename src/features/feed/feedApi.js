2fcca5d746521d214d233b15951b352d7d0a2ee3
wpierce19 <126428398+wpierce19@users.noreply.github.com>
2025-10-03 13:54:08 -0600

    created outline of post retrieving api calls

--- src/features/feed/feedApi.js
+++ src/features/feed/feedApi.js
@@ -1,1 +1,55 @@
 //place api functions here for feed fetching
+import { useAuthStore } from '../auth/authStore';
+
+export const getFriendsPosts = async (token) => {
+    try {
+        const response = await fetch('/api/friends/posts', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        if (!response.ok) {
+            const errorData = await response.json();
+            throw new Error(errorData.message || 'Failed to fetch friends posts');
+        }
+        const data = await response.json();
+        console.log('Friends posts fetched:', data);
+    }
+    catch (error) {
+        console.error('Error fetching friends posts:', error);
+    }
+}
+
+export const getPosts = async () => {
+    const response = await fetch('/api/posts', {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+    });
+    const data = await response.json();
+    console.log("Posts fetched:", data);
+}
+
+export const createPost = async (newPost) => {
+    const { token } = useAuthStore.getState(); // Access Zustand outside React
+    if (!token) {
+        throw new Error('User not authenticated');
+    }
+    try {
+        const response = await fetch('/api/newPosts', {
+            method: 'POST',
+            body: JSON.stringify(newPost),
+            headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${token}` },
+        });
+        if (!response.ok) {
+            const errorData = await response.json();
+            throw new Error(errorData.message || 'Failed to create post');
+        }
+        const data = await response.json();
+        console.log('Post created successfully', data);
+    }
+    catch (error) {
+        console.error('Error creating post:', error);
+    }
+}
