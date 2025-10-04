8083557b2cb340b576d9bc236114c95e9a43f60e
wpierce19 <126428398+wpierce19@users.noreply.github.com>
2025-10-04 15:50:06 -0600

    query hooks for profile complete

---
+++ src/features/profile/profileHooks.js
@@ -0,0 +1,31 @@
+import { useQuery, useMutation } from "@tanstack/react-query";
+import { updateProfile, uploadAvatar, fetchPublicProfile, refetchProfile } from "./profileApi";
+
+
+export const useGetPublicProfile = (id) => {
+    return useQuery({
+        queryKey: ['publicProfile', id],
+        queryFn: () => fetchPublicProfile(id),
+        enabled: !!id,
+    });
+}
+
+export const useRefetchProfile = () => {
+    return useQuery({
+        queryKey: ['profile'],
+        queryFn: () => refetchProfile(),
+        staleTime: 5 * 60 * 1000, // 5 minutes
+    });
+}
+
+export const useUploadAvatar = () => {
+    return useMutation({
+        mutationFn: (file) => uploadAvatar(file),
+    });
+}
+
+export const useUpdateProfile = () => {
+    return useMutation({
+        mutationFn: (form) => updateProfile(form),
+    });
+}
